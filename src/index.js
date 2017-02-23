8bfbc3aee8a8a7a110cf4ae3a87caaede07fff43
hamt3ch <hmiles23@ufl.edu>
2017-02-23 00:38:50 -0800

    basic post and get working with micro

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-export default (req, res) => 'Welcome to micro'
+import match from 'micro-match';
+import { send } from 'micro';
+import 'babel-core/register';
+import 'babel-polyfill';
+
+export default async (req, res) => {
+
+  const userid = match('/:id', req.url)
+  const method = req.method
+
+  // query user information an return current location
+  if(req.method === 'GET'){
+    return send(res, 200, {'foo': 'get'})
+  }
+
+  // set current users current location
+  if(req.method === 'POST') {
+    return send(res, 200, {'foo': 'post'})
+  }
+
+  return send(res, 404);
+}
